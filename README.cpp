3e4f4fedcdb9c2704d7f9c472bfd175262af2b2b
jiaqi <2412467996@qq.com>
2020-08-25 17:32:33 +0800

    Update README.cpp

--- README.cpp
+++ README.cpp
@@ -3,7 +3,7 @@
  * 1. 回溯模版 ：回溯.cpp  组合排列.cpp
  * 回溯知识点总结：https://leetcode-cn.com/problems/subsets/solution/c-zong-jie-liao-hui-su-wen-ti-lei-xing-dai-ni-gao-/
  *      全排列，组合，子集，电话号码
- *      字符串切割，八皇后，铺地砖，数独
+ *      字符串切割，八皇后，铺地砖(未做)，数独(未做)
  *      机器人运动系列问题：这里面使用的是回溯，注意和动态规划中最大路径数目的区分
  * 2. 动态规划系列
  * 3. 重复区间 ：贪心.cpp
@@ -17,4 +17,5 @@
 //前缀树
 //线段树
 //滑动窗口
+//
 #endif
